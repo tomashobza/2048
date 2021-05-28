d1101385d449dd5be61fe52c36e1460d4bb14e75
tomashobza <hobza.tomas@gmail.com>
2021-05-28 08:57:32 +0200

    add: code documentation

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,21 @@ let pole = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0]
-];
+]; // dvojrozměrné pole s hodnotami jednotlivých políček
 
-let score = 0;
+let score = 0; // počítač skore
 
 let leaderboard = [
-    ["Bláža", 69],
-    ["Martíněk", 420],
-];
+    ["Kebabláža", 69],
+    ["Špízřík", 420],
+]; // pole se záznamy z výsledkové listiny, naplněno testovacími daty
 
-let jmeno = "Noname";
+let jmeno = "Noname"; // default pro jméno
 
+/**
+ * Změní skóre na nové a vypíše jej na dané místo
+ * @param noveScore - Celé číslo, nové skóre
+ */
 const zmenScore = (noveScore) => {
     score = noveScore;
     document.querySelector("#scoreNum").innerHTML = score;
@@ -22,6 +26,9 @@ const zmenScore = (noveScore) => {
 
 // pole [x] [y]
 
+/**
+ * Projede všechny políčka a zapíše do nich jejich nové hodnoty z abstraktního hracího pole
+ */
 const vykresliPole = () => {
     document.querySelectorAll('#container>div').forEach((square, i) => {
         square.innerHTML = pole[Math.floor(i / 4)][Math.floor(i % 4)] == 0 ? "" : pole[Math.floor(i / 4)][Math.floor(i % 4)];
@@ -29,6 +36,9 @@ const vykresliPole = () => {
     })
 };
 
+/**
+ * Nahrané body zapíše do výsledkové listiny, nastaví pole a skóre na nuly a náhodně umístí dvě políčka o hodnotě 2
+ */
 const novaHra = () => {
     if (score > 0) {
         leaderboard.push([jmeno, score]);
@@ -53,6 +63,11 @@ const novaHra = () => {
     vykresliPole();
 }
 
+/**
+ * Nejdřív přemaže a pak vypíše výsledkovou listinu
+ * @param jmeno - Jméno hráče
+ * @param score - Hráčovo skóre
+ */
 const vykresliVysledky = (jmeno, score) => {
     leaderboard = leaderboard.sort((a, b) => {
         return b[1] - a[1]
@@ -70,6 +85,9 @@ const vykresliVysledky = (jmeno, score) => {
     document.querySelector("#leaderboard-content").appendChild(li);
 };
 
+/**
+ * Přidá políčko o hodnotě 2 na náhodné nulové políčko
+ */
 const pridejCtv = () => {
     let newRand = Math.floor(Math.random() * 16);
     while (pole[Math.floor((newRand % 16) / 4)][Math.floor((newRand % 16) % 4)] != 0) {
@@ -78,6 +96,13 @@ const pridejCtv = () => {
     pole[Math.floor((newRand % 16) / 4)][Math.floor((newRand % 16) % 4)] = 2;
 }
 
+
+/**
+ * Posune políčko o jedno v daném směru
+ * @param x - X-ová souřadnice posouvaného políčka 
+ * @param y - Y-ová souřadnice posouvaného políčka
+ * @param smer - Směr tahu (NA || DO || VL || VP)
+ */
 const posunOJeden = (x, y, smer) => {
     if (smer == "nahoru" && x > 0) {
         pole[x - 1][y] = pole[x][y];
@@ -97,6 +122,11 @@ const posunOJeden = (x, y, smer) => {
     }
 }
 
+/**
+ * Posune políčko nahoru, po nejblížší nenulové políčko, či okraj
+ * @param x - X-ová souřadnice posouvaného políčka 
+ * @param y - Y-ová souřadnice posouvaného políčka
+ */
 const posunCtvNahoru = (x, y) => {
     if (pole[x][y] != 0 && x > 0) {
         while (x > 0 && pole[x - 1][y] == 0) {
@@ -111,6 +141,11 @@ const posunCtvNahoru = (x, y) => {
     }
 };
 
+/**
+ * Posune políčko dolů, po nejblížší nenulové políčko, či okraj
+ * @param x - X-ová souřadnice posouvaného políčka 
+ * @param y - Y-ová souřadnice posouvaného políčka
+ */
 const posunCtvDolu = (x, y) => {
     if (pole[x][y] != 0 && x < 3) {
         while (x < 3 && pole[x + 1][y] == 0) {
@@ -125,6 +160,11 @@ const posunCtvDolu = (x, y) => {
     }
 };
 
+/**
+ * Posune políčko vlevo, po nejblížší nenulové políčko, či okraj
+ * @param x - X-ová souřadnice posouvaného políčka 
+ * @param y - Y-ová souřadnice posouvaného políčka
+ */
 const posunCtvVlevo = (x, y) => {
     if (pole[x][y] != 0 && y > 0) {
         while (y > 0 && pole[x][y - 1] == 0) {
@@ -139,6 +179,11 @@ const posunCtvVlevo = (x, y) => {
     }
 };
 
+/**
+ * Posune políčko vpravo, po nejblížší nenulové políčko, či okraj
+ * @param x - X-ová souřadnice posouvaného políčka 
+ * @param y - Y-ová souřadnice posouvaného políčka
+ */
 const posunCtvVpravo = (x, y) => {
     if (pole[x][y] != 0 && y < 3) {
         while (y < 3 && pole[x][y + 1] == 0) {
@@ -153,6 +198,9 @@ const posunCtvVpravo = (x, y) => {
     }
 };
 
+/**
+ * Pro všecha políčka provede posunutí nahoru
+ */
 const nahoru = () => {
     for (let j = 0; j < 4; j++) {
         for (let i = 0; i < 4; i++) {
@@ -163,6 +211,9 @@ const nahoru = () => {
     vykresliPole();
 };
 
+/**
+ * Pro všecha políčka provede posunutí dolů
+ */
 const dolu = () => {
     for (let j = 0; j < 4; j++) {
         for (let i = 3; i >= 0; i--) {
@@ -173,6 +224,9 @@ const dolu = () => {
     vykresliPole();
 };
 
+/**
+ * Pro všecha políčka provede posunutí vlevo
+ */
 const vlevo = () => {
     for (let j = 0; j < 4; j++) {
         for (let i = 0; i < 4; i++) {
@@ -183,6 +237,9 @@ const vlevo = () => {
     vykresliPole();
 };
 
+/**
+ * Pro všecha políčka provede posunutí vpravo
+ */
 const vpravo = () => {
     for (let j = 0; j < 4; j++) {
         for (let i = 3; i >= 0; i--) {
@@ -193,17 +250,21 @@ const vpravo = () => {
     vykresliPole();
 };
 
+// Posluchače na kliknutí na herní tlačítka
 document.querySelector('#Nahoru').addEventListener('click', nahoru);
 document.querySelector('#Dolu').addEventListener('click', dolu);
 document.querySelector('#Vlevo').addEventListener('click', vlevo);
 document.querySelector('#Vpravo').addEventListener('click', vpravo);
 
+// Posluchače na kliknutí na tlačítko nové hry
 document.querySelector('#Nova-hra-btn').addEventListener('click', novaHra);
 
+// Posluchač na změnu vstupu pro jméno
 document.querySelector('#name').addEventListener('change', () => {
     jmeno = document.querySelector('#name').value;
     vykresliVysledky(jmeno, score);
 });
 
+// počátek první hry a vykreslení abstraktního pole
 novaHra();
 vykresliPole();
